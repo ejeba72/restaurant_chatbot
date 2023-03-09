53aa8e64725bf3497bf78c34926638e6c897c19d
Emmanuel Eni <eejeba@gmail.com>
2023-03-09 12:48:31 +0100

    Warm up: setting up a basic express server

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { config } = require('dotenv');
 const http = require('http');
 const { print } = require('./helpers/print');
+const express = require('express');
 
 config();
 
-const hostname = '127.0.0.1';
 const PORT = process.env.PORT;
 
-print.info(PORT);
+// const server = http.createServer((req, res) => {
+//   res.end('Everwhere stew!')
+// });
 
-const server = http.createServer((req, res) => {
-  res.end('Everwhere stew!')
-});
+const app = express();
 
-server.listen(PORT, hostname, () => {
-  print.info('Server is attentively listening to request @ ' + hostname + ':' + PORT);
+app.get('/', (req, res) => {
+  try {
+    print.info('response is successful')
+    res.status(200).send({
+      response: "helloOoo, world!!!"
+    })
+  } catch (err) {
+    print.error('Mayday, mayday, we have a problem!');
+    res.status(500).send('we have a problem');
+  }
+})
+
+app.listen(PORT, () => {
+  print.info('Server is attentively listening to request @ 127.0.0.1:' + PORT);
 })
