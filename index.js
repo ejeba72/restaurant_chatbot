f4bd912cd9f0d0e06c4edcd9453059f4ab95eb8e
Emmanuel Eni <eejeba@gmail.com>
2023-03-15 12:13:05 +0100

    code cleanup

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const { config } = require('dotenv');
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const { welcomeMsg, 
+const { 
+  welcomeMsg, 
   restaurantMenu, 
-  /*riceFlavour,*/ 
-  showInfo, 
   meatList,
   checkout,
   orderPlaced,
@@ -19,11 +18,9 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 
 let riceFlavour, meatType;
 const customerOrder = []
@@ -41,74 +38,48 @@ io.on('connection', (socket) => {
 
   socket.on('client to server', (msg) => {
     print.info(`client message: ${msg}`);
-    // io.emit('eventName', data);
-    // io.emit('server to client', msg);
-    
-    // const customer = {
-    //   customerId: socket.id,
-    //   customerOrder: []
-    // }
-    // const customerOrder = []
 
     if (msg === '0') {
       print.info(`customer with id, ${socket.id}, has cancelled order`)
       io.emit('server to client', `You have cancelled your order. Select 1 to place a new order`)
 
     } else if (msg === '1') {
-      // const customerOrder = [];
       io.emit('server to client', restaurantMenu)
 
     } else if (msg === '2') {
       riceFlavour = 'Rice and Pepper Stew';
-      customerOrder.push(riceFlavour);
-      print.info(showInfo(riceFlavour, customerOrder, meatList));
       io.emit('server to client', meatList(riceFlavour))
 
     } else if (msg === '3') {
       riceFlavour = 'Jollof rice and Moi-moi';
-      customerOrder.push(riceFlavour);
-      print.info(showInfo(riceFlavour, customerOrder, meatList));
       io.emit('server to client', meatList(riceFlavour));
 
     } else if (msg === '4') {
       riceFlavour = 'Fried Rice and Salad';
-      customerOrder.push(riceFlavour);
-      print.info(showInfo(riceFlavour, customerOrder, meatList));
       io.emit('server to client', meatList(riceFlavour));
 
     } else if (msg === '5') {
       meatType = 'CatFish';
-      customerOrder.push(meatType);
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
 
     } else if (msg === '6') {
       meatType = 'Chicken lap';
-      customerOrder.push(meatType);
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
       
     } else if (msg === '7') {
       meatType = 'Goat meat';
-      customerOrder.push(meatType);
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
 
     } else if (msg === '8') {
       meatType = 'Beef';
-      customerOrder.push(meatType);
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
 
     } else if (msg === '9') {
       meatType = 'Assorted';
-      customerOrder.push(meatType);
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
 
     } else if (msg === '10') {
       // customer doesn't want meat
-      print.info(showInfo(meatType, customerOrder, checkout));
       io.emit('server to client', checkout());
 
     } else if (msg === '97') {
@@ -121,105 +92,14 @@ io.on('connection', (socket) => {
       // When a customer selects “98”, the bot should be able to return all placed order
 
     } else if (msg === '99') {
-      print.info(showInfo(meatType, customerOrder, orderPlaced));
       io.emit('server to client', orderPlaced(riceFlavour, meatType))
+      
     } else {
       io.emit('server to client', 'Please enter a valid response');
     }
   })
-
-  print.info(customerOrder);
-
 });
 
 server.listen(PORT, () => {
   print.info('server is attentively listening @ 127.0.0.1:' + PORT)
-  // chatbotLogic();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const { config } = require('dotenv');
-// const { createServer } = require('http');
-// const { print } = require('./helpers/print');
-
-// config();
-// const PORT = process.env.PORT;
-// const app = express();
-// const server = createServer(app);
-
-
-
-// // app.get('/', (req, res) => {
-// //   res.send({
-// //     transformer: 'My name is Optimum Prime.'
-// //   })
-// // })
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// })
-
-// server.listen(PORT, () => {
-//   print.info('Server is attentively listening for requests @ 127.0.0.1:' + PORT + '.');
-// })
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const { config } = require('dotenv');
-// const { createServer } = require('http');
-// const { print } = require('./helpers/print');
-
-// config();
-// // const app = express();
-
-// const PORT = process.env.PORT;
-
-// const app = createServer((req, res) => {
-//     res.end('Everwhere stew!')
-//   })
-
-// // const server = http.createServer((req, res) => {
-// //   res.end('Everwhere stew!')
-// // });
-
-// // app.get('/', (req, res) => {
-// //   try {
-// //     print.info('response is successful')
-// //     res.status(200).send({
-// //       response: "helloOoo, world!!!"
-// //     })
-// //   } catch (err) {
-// //     print.error('Mayday, mayday, we have a problem!');
-// //     res.status(500).send('we have a problem');
-// //   }
-// // })
-
-// app.listen(PORT, () => {
-//   print.info('Server is attentively listening to request @ 127.0.0.1:' + PORT);
-// })
--- messages.js
+++ messages.js
@@ -1,88 +1,51 @@
-// const riceFlavour = '';
+module.exports = {
+  welcomeMsg: `
+    Hey there!
+    Welcome to Mama Ebo Pepper Rice!
+    Please select an option from the list:
+    Select 1 to Place an order,
+    Select 0 to cancel order
+  `,
 
-const welcomeMsg = `
-Hey there!
-Welcome to Mama Ebo Pepper Rice!
-Please select an option from the list:
-Select 1 to Place an order,
-Select 0 to cancel order`;
+  restaurantMenu: `
+    Thank you for opting to place an order.
+    Here is our menu:
+    Select 2 for White Rice and Pepper Stew,
+    Select 3 for Jollof rice and Moi-moi,
+    Select 4 for Fried Rice and Salad,
+    Select 98 to see order history,
+    Select 97 to see current order,
+    Select 0 to cancel order
+  `,
 
-const restaurantMenu = `
-Thank you for opting to place an order.
-Here is our menu:
-Select 2 for Rice and Pepper Stew,
-Select 3 for Jollof rice and Moi-moi,
-Select 4 for Fried Rice and Salad,
-Select 98 to see order history,
-Select 97 to see current order,
-Select 0 to cancel order
-`
+  meatList: (riceFlavour) => { 
+    return `
+      What would you garnish your ${riceFlavour} with?
+      Select 5 for CatFish,
+      Select 6 for Chicken lap,
+      Select 7 for Goat meat, 
+      Select 8 for Beef,
+      Select 9 for Assorted,
+      Select 10 if you don't want any of them,
+      Select 98 to see order history,
+      Select 97 to see current order,
+      Select 0 to cancel order
+    `
+  },
 
-function meatList(riceFlavour) { 
-  return `
-  What would you garnish your ${riceFlavour} with?
-  Select 5 for CatFish,
-  Select 6 for Chicken lap,
-  Select 7 for Goat meat, 
-  Select 8 for Beef,
-  Select 9 for Assorted,
-  Select 10 if you don't want any of them,
-  Select 98 to see order history,
-  Select 97 to see current order,
-  Select 0 to cancel order
-  `
+  checkout: () => { 
+    return `
+      Select 99 to checkout,
+      Select 98 to see order history,
+      Select 97 to see current order,
+      Select 0 to cancel order
+    `
+  },
+
+  orderPlaced: (riceFlavour, meatType) => {
+    return `
+      Your order for ${riceFlavour}, garnished with ${meatType} has been placed, successfully.
+      Select 1 to place another order.
+    `
+  },
 }
-
-function checkout() { 
-  return `
-  Select 99 to checkout,
-  Select 98 to see order history,
-  Select 97 to see current order,
-  Select 0 to cancel order
-  `
-}
-
-function orderPlaced(riceFlavour, meatType) {
-  return `
-  Your order for ${riceFlavour}, garnished with ${meatType} has been placed, successfully.
-  Select 1 to place another order.
-  `
-}
-
-// Your order has been placed for 
-
-function showInfo(riceFlavour, customerOrder, meatList) {
-  return `
-  riceFlavour ==> ${riceFlavour},
-  customerOrder ==> ${customerOrder},
-  meatList ==> ${meatList}
-  `
-}
-
-module.exports = { 
-  /*riceFlavour,*/ 
-  welcomeMsg, 
-  restaurantMenu, 
-  meatList, 
-  checkout,
-  orderPlaced, 
-  showInfo 
-}
-
-
-
-// const placeOrder = 
-// `Select 99 to checkout order
-// Select 98 to see order history
-// Select 97 to see current order
-// Select 0 to cancel order`
-
-const moreReserve =
-`5 Rice and oil beans
-6 Rice and dry beans
-7 Coconut rice
-8 Banger rice`
-
-// const riceAndStewOptions = `
-// Select 5 
-// select x for none of the options`
