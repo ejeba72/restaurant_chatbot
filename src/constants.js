5213a2fcf4005081f81d913fddc6813566eda038
Emmanuel Eni <eejeba@gmail.com>
2023-03-17 20:14:41 +0100

    created a 'constants.js' file that contains the Rice flavours and meat types that customers can choose from. BTW, I didn't declare my variables in the 'chatbot.controller.js' file (i.e. 'riceFlavour' and 'meatType' variables). Yet, I didn't get any error warnings from VS Code or Node. All this while, everything appeared to be working just fine as expected! Speaking of 'ignorant bliss'! :)

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// This file contains the Rice Flavours and Meat Types, which customers can choose from.
+
+module.exports.RICE = {
+  pepperRice: 'White Rice and Pepper Stew',
+  jollofRice: 'Jollof rice and Moi-moi',
+  friedRice: 'Fried Rice and Salad',
+};
+
+module.exports.MEAT = {
+  catfish: 'Cat Fish',
+  chicken: 'Chicken lap',
+  goat: 'Goat meat',
+  beef: 'Beef',
+  assorted: 'Assorted'
+}
